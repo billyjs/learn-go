4e349c9a4052b9503b347401fd431d7c10f6040b
Billy Schulze <bschulze@atlassian.com>
2022-07-15 14:54:12 +1000

    Call greetings module from hello
    Signed-off-by: Billy Schulze <bschulze@atlassian.com>

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-import "rsc.io/quote"
+	"example.com/greetings"
+)
 
 func main() {
-	fmt.Println(quote.Go())
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
